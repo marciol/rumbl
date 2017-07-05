0dac5ffd4147887c89720b3431b12d86f0d3fd18
Marcio L. de Faria <marcitolopes@gmail.com>
2017-07-04 23:12:39 -0300

    fix typo

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -14,7 +14,7 @@ defmodule Rumbl.Auth do
     cond do
       user = conn.assigns[:current_user] ->
         conn
-      user = user_id && repo.get(Rumbl.User, user_id)
+      user = user_id && repo.get(Rumbl.User, user_id) ->
         assign(conn, :current_user, user)
       true ->
         assign(conn, :current_user, nil)
@@ -23,7 +23,7 @@ defmodule Rumbl.Auth do
 
   def login(conn, user) do
     conn
-    |> assign(:current_user_, user)
+    |> assign(:current_user, user)
     |> put_session(:user_id, user.id)
     |> configure_session(renew: true)
   end
